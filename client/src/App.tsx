0c9cf9a815654c5a4a4f15ff7b9a112d51474f23
Justin Kelley <pingjustin@gmail.com>
2025-03-03 20:45:49 -0500

    Integrated authentication hook & context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,1 +1,20 @@
-//main app component
+//main app componentimport React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/home';
+import ResumeInput from './pages/resumeInput';
+
+const App = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/resumeInput" element={<ResumeInput />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+};
+
+export default App;
