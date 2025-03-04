075cb646b7861dfe8329a421d5b132ada6ef1274
marylfisher <marylaurenfisher@gmail.com>
2025-03-04 13:52:23 -0700

    navbar bootstrap integration

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Navbar() {
+function CustomNavbar() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav justify-content-end mb-2">
+    <Navbar className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">ResuMate</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+   <div className="collapse navbar-collapse" id="navbarText">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Resume Input</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Cover Letter</a>
+      </li>
+    </ul>
+    
+  </div>
+
+
+    {/* <ul className="nav justify-content-end mb-2">
       <li className="nav-item">
         <Link
           to="/"
@@ -54,8 +81,10 @@ function Navbar() {
           Cover Lettter
         </Link>
       </li>
-    </ul>
+    </ul> */}
+    </Navbar>
+  
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button as BootstrapButton } from 'react-bootstrap';
+import {Button as BootstrapButton } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface ButtonProps {
