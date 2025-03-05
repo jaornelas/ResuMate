1b01636ccd1d2affa8ab8260a8db0577cc0ea816
marylfisher <marylaurenfisher@gmail.com>
2025-03-04 17:09:45 -0700

    added header and footer base

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+    return(
+        <footer>
+            <Container>
+
+            </Container>
+        </footer>
+    )
+
+}
+
+export default Footer;
