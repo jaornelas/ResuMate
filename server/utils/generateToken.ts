e2b356c00fef44af4a39c88fc79839e3bb26dfc5
Megan Ward <wardmeg1998@gmail.com>
2025-03-04 20:35:12 -0500

    generateToken

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -1,1 +1,16 @@
-//utility functions and jwt token generator
+//jwt token generator
+import jwt from "jsonwebtoken";
+
+/**
+ * @param {number} userId 
+ * @returns {string} - generated token
+ */
+const generateToken = (userId: number): string => {
+  const SECRET_KEY: string = process.env.SECRET_KEY || "your_jwt_secret";
+
+  return jwt.sign({ userId }, SECRET_KEY, {
+    expiresIn: "1h", 
+  });
+};
+
+export default generateToken;
