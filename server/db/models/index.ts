11a83af06186005c16a88ee1c84621b7d3833595
Megan Ward <wardmeg1998@gmail.com>
2025-03-06 19:05:23 -0500

    added content to index.ts, edited all models to ensure conenction to sequelize

--- server/db/models/index.ts
+++ server/db/models/index.ts
@@ -1,1 +1,11 @@
 //exports all models
+import { Sequelize } from "sequelize";
+import sequelize from "../config/connection";
+import User from "./user";
+import Resume from "./resume";
+
+// Define model relationships
+User.hasMany(Resume, { foreignKey: "userId", onDelete: "CASCADE" });
+Resume.belongsTo(User, { foreignKey: "userId" });
+
+export { sequelize, User, Resume };
