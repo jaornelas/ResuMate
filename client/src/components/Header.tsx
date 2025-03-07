c62b01f724e1b188cf63e607d08f19f398199538
marylfisher <marylaurenfisher@gmail.com>
2025-03-06 17:02:45 -0700

    footer, header, logon form

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Footer() {
+function Header() {
     return(
         <header>
             <Container>
@@ -13,4 +13,4 @@ function Footer() {
 
 }
 
-export default Footer;
+export default Hooter;
